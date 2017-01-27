ea0664b34e0c64ce3101dd780f7b7b8cc848aa30
Haoran Sun <hsun960120@gmail.com>
2017-01-27 02:09:45 -0800

    Format fix

--- docTagger/docTagger.java
+++ docTagger/docTagger.java
@@ -21,174 +21,174 @@ import com.opencsv.*;
  * A class that implement a document tagger (tag Chinese words with their
  * English meaning) using a hash multi-map and self-balanced interval search
  * tree.
- *  
+ * 
  * @author Haoran Sun
  * @since 01/26/2017
  */
 public class docTagger {
-	static final int CHN_IDX = 2;
-	static final int OUTPUT_IDX = 2;
-	static final int FREQ_CAP = 35; //The size of frequency array
-	static final String FMT = "UTF-8";
-	static final String TAG = "<%s>%s</%s>"; //Tag formatter
-	
-	private HashMultimap<String, String> dict;
-	private int[] freq; //Array that stores the frequency of word lengths
-	
-	/**
-	 * Constructor
-	 */
-	public docTagger() {
-		this.dict = HashMultimap.create();
-		this.freq = new int[FREQ_CAP];
-	}
-	
-	/**
-	 * Load the whole dictionary to a hash multi-map
-	 * @param dictName the dictionary file name
-	 */
-	public void loadDict(String dictName) {
-		InputStreamReader in;
-		CSVReader reader;
-		try { /* Read and hash to the multi-map */
-			in = new InputStreamReader(new FileInputStream(dictName), FMT);
-			reader = new CSVReader(in);
-			String[] dictLine;
-			/* Use Chinese words as keys and English words as values */
-			while((dictLine = reader.readNext()) != null) {
-				if(dictLine[1].contains("~"))
-					dictLine[0] = dictLine[1].replace("~", dictLine[0]);
-				this.dict.put(dictLine[CHN_IDX], dictLine[0]);
-				/* Update frequency in terms of word lengths */
-				freq[dictLine[CHN_IDX].length()]++;
-			}
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Print the distribution of word lengths of the dictionary
-	 */
-	public void getStats() {
-		for(int i = 1; i < freq.length; i++) {
-			System.out.println(i + ": " + freq[i]);
-		}
-	}
-	
-	/**
-	 * Read the document and output tagged text using n-gram matching
-	 * @param docName filename of the input document
-	 * @param outFile output filename
-	 * @param n the length of the longest word to match
-	 */
-	public void process(String docName, String outFile, int n) {
-		try {
-			Scanner sc = new Scanner(new FileInputStream(docName), FMT);
-			/*
-			 * Note: still need to think about how to process segments
-			 * May be need to split by "," as well
-			 */
-			String[] content = sc.next().split("。");
-			sc.close();
-			PrintWriter writer = new PrintWriter(new OutputStreamWriter
-					(new FileOutputStream(outFile), FMT));
-			int wordLength = n;
-			
-			/* Iterate through all sentences */
-			for(int i = 0; i < content.length; i++) {
-				IntervalTree<IndexInterval> ist = new IntervalTree<>();
-				int endIndex = 0;
-				wordLength = n;
-				
-				/* First round word matching using the word length n */
-				for(int j = 0; j < content[i].length();) {
-					endIndex = j + wordLength - 1;
-					if(endIndex >= content[i].length()) break;
-					
-					/* Word matching */
-					if(this.dict.containsKey(content[i].substring(j,
-							endIndex + 1))) {
-						/* Mark interval */
-						ist.insert(new IndexInterval(j, endIndex));
-						j = endIndex + 1; //Get next index
-					}else {
-						j++;
-					}
-				}
-				wordLength--; //Ready to match word with shorter length
-				
-				int nextPos;
-				for(; wordLength > 1; wordLength--) { //Try matching all words until the length 2
-					/* Start from the first available position */
-					for(int j = ist.nextAvailable(new IndexInterval(0, wordLength - 1));
-							j < content[i].length();) {
-						endIndex = j + wordLength - 1;
-						if(endIndex >= content[i].length()) break;
-						
-						nextPos = ist.nextAvailable(new IndexInterval(j,
-								endIndex)); //Check overlap
-						if(j == nextPos) { //No overlap found
-							/* Word matching */
-							if(this.dict.containsKey(content[i].substring(j,
-									endIndex + 1))) {
-								/* Mark interval */
-								ist.insert(new IndexInterval(j, endIndex));
-								j = endIndex + 1; //Get next index
-							}else {
-								j++;
-							}
-						}else
-							j = nextPos; //Try next available position
-					}
-				}
-				
-				/* Start output */
-				Set<String> matchWords;
-				String enWord;
-				String cnWord;
-				for(int j = 0; j < content[i].length();) {
-					nextPos = ist.nextAvailable(new IndexInterval(j, j));
-					if(nextPos == j) { //Single character is not matched yet
-						matchWords = this.dict.get(content[i].charAt(j) + "");
-						/* No match found */
-						if(matchWords.isEmpty())
-							writer.print(content[i].charAt(j));
-						else {
-							/* Get the first match */
-							enWord = (String) matchWords.toArray()[0];
-							writer.print(String.format(TAG, enWord,
-									content[i].charAt(j), enWord));
-						}
-						j++;
-					}else {
-						/* Obtain the Chinese word */
-						cnWord = content[i].substring(j, nextPos);
-						matchWords = this.dict.get(cnWord);
-						enWord = (String) matchWords.toArray()[0];
-						writer.print(String.format(TAG, enWord, cnWord,
-								enWord));
-						j = nextPos; //Skip to the start of the next word
-					}
-				}
-				writer.print("。");
-			}
-			writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Driver of the document tagger
-	 * @param args command line arguments
-	 */
-	public static void main(String args[]) {
-		docTagger tagger = new docTagger();
-		tagger.loadDict(args[0]);
-		System.out.println("Dictionary loaded.");
-		tagger.getStats();
-		tagger.process(args[1], args[OUTPUT_IDX], 12);
-	}
+  static final int CHN_IDX = 2;
+  static final int OUTPUT_IDX = 2;
+  static final int FREQ_CAP = 35; // The size of frequency array
+  static final String FMT = "UTF-8";
+  static final String TAG = "<%s>%s</%s>"; // Tag formatter
+
+  private HashMultimap<String, String> dict;
+  private int[] freq; // Array that stores the frequency of word lengths
+
+  /**
+   * Constructor
+   */
+  public docTagger() {
+    this.dict = HashMultimap.create();
+    this.freq = new int[FREQ_CAP];
+  }
+
+  /**
+   * Load the whole dictionary to a hash multi-map
+   * 
+   * @param dictName - the dictionary file name
+   */
+  public void loadDict(String dictName) {
+    InputStreamReader in;
+    CSVReader reader;
+    try { /* Read and hash to the multi-map */
+      in = new InputStreamReader(new FileInputStream(dictName), FMT);
+      reader = new CSVReader(in);
+      String[] dictLine;
+      /* Use Chinese words as keys and English words as values */
+      while ((dictLine = reader.readNext()) != null) {
+        if (dictLine[1].contains("~"))
+          dictLine[0] = dictLine[1].replace("~", dictLine[0]);
+        this.dict.put(dictLine[CHN_IDX], dictLine[0]);
+        /* Update frequency in terms of word lengths */
+        freq[dictLine[CHN_IDX].length()]++;
+      }
+      reader.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Print the distribution of word lengths of the dictionary
+   */
+  public void getStats() {
+    for (int i = 1; i < freq.length; i++) {
+      System.out.println(i + ": " + freq[i]);
+    }
+  }
+
+  /**
+   * Read the document and output tagged text using n-gram matching
+   * 
+   * @param docName - filename of the input document
+   * @param outFile - output filename
+   * @param n - the length of the longest word to match
+   */
+  public void process(String docName, String outFile, int n) {
+    try {
+      Scanner sc = new Scanner(new FileInputStream(docName), FMT);
+      /*
+       * Note: still need to think about how to process segments May be need to
+       * split by "," as well
+       */
+      String[] content = sc.next().split("。");
+      sc.close();
+      PrintWriter writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(outFile), FMT));
+      int wordLength = n;
+
+      /* Iterate through all sentences */
+      for (int i = 0; i < content.length; i++) {
+        IntervalTree<IndexInterval> ist = new IntervalTree<>();
+        int endIndex = 0;
+        wordLength = n;
+
+        /* First round word matching using the word length n */
+        for (int j = 0; j < content[i].length();) {
+          endIndex = j + wordLength - 1;
+          if (endIndex >= content[i].length())
+            break;
+
+          /* Word matching */
+          if (this.dict.containsKey(content[i].substring(j, endIndex + 1))) {
+            /* Mark interval */
+            ist.insert(new IndexInterval(j, endIndex));
+            j = endIndex + 1; // Get next index
+          } else {
+            j++;
+          }
+        }
+        wordLength--; // Ready to match word with shorter length
+
+        int nextPos;
+        for (; wordLength > 1; wordLength--) { // Try matching all words until
+                                               // the length 2
+          /* Start from the first available position */
+          for (int j = ist.nextAvailable(new IndexInterval(0, wordLength - 1)); j < content[i].length();) {
+            endIndex = j + wordLength - 1;
+            if (endIndex >= content[i].length())
+              break;
+
+            nextPos = ist.nextAvailable(new IndexInterval(j, endIndex)); // Check
+                                                                         // overlap
+            if (j == nextPos) { // No overlap found
+              /* Word matching */
+              if (this.dict.containsKey(content[i].substring(j, endIndex + 1))) {
+                /* Mark interval */
+                ist.insert(new IndexInterval(j, endIndex));
+                j = endIndex + 1; // Get next index
+              } else {
+                j++;
+              }
+            } else
+              j = nextPos; // Try next available position
+          }
+        }
+
+        /* Start output */
+        Set<String> matchWords;
+        String enWord;
+        String cnWord;
+        for (int j = 0; j < content[i].length();) {
+          nextPos = ist.nextAvailable(new IndexInterval(j, j));
+          if (nextPos == j) { // Single character is not matched yet
+            matchWords = this.dict.get(content[i].charAt(j) + "");
+            /* No match found */
+            if (matchWords.isEmpty())
+              writer.print(content[i].charAt(j));
+            else {
+              /* Get the first match */
+              enWord = (String) matchWords.toArray()[0];
+              writer.print(String.format(TAG, enWord, content[i].charAt(j), enWord));
+            }
+            j++;
+          } else {
+            /* Obtain the Chinese word */
+            cnWord = content[i].substring(j, nextPos);
+            matchWords = this.dict.get(cnWord);
+            enWord = (String) matchWords.toArray()[0];
+            writer.print(String.format(TAG, enWord, cnWord, enWord));
+            j = nextPos; // Skip to the start of the next word
+          }
+        }
+        writer.print("。");
+      }
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Driver of the document tagger
+   * 
+   * @param args - command line arguments
+   */
+  public static void main(String args[]) {
+    docTagger tagger = new docTagger();
+    tagger.loadDict(args[0]);
+    System.out.println("Dictionary loaded.");
+    tagger.getStats();
+    tagger.process(args[1], args[OUTPUT_IDX], 12);
+  }
 }
--- hashJoin.java
+++ hashJoin.java
@@ -14,173 +14,175 @@ import com.google.common.collect.*;
 
 /**
  * A class that implement double-pipelined hash join with multi-core support.
+ * 
  * @author Haoran Sun
  * @since 01/14/2017
  */
 public class hashJoin {
-	static final int SIZE = 2003;
-	static final int FREQ_CAP = 35;
-	static final int CHN_IDX = 2;
-	static final int OUTPUT_IDX = 2;
-	static final String FMT = "UTF-8";
-	
-	//private Multimap dict;
-	private HashMultimap<String, String> dict;
-	private Hashtable<String, String> doc;
-	private PrintWriter writer;
-	
-	/**
-	 * Default constructor
-	 */
-	public hashJoin() {
-		//this.dict = new Multimap();
-		this.dict = HashMultimap.create();
-		this.doc = new Hashtable<String, String>(SIZE);
-	}
-	
-	/**
-	 * Helper method to probe and insert data
-	 * @param key Chinese word
-	 * @param word English word
-	 * @param option 0 to insert to the document hashtable; 1 to insert to the
-	 * dictioanry hashtable
-	 */
-	private void probeAndInsert(String key, String word, int option) {
-		if(option == 0) { //Probe the dictionary and insert to document
-			Set<String> engWords = this.dict.get(key);
-			Iterator<String> it = engWords.iterator();
-			String w;
-			while(it.hasNext()) {
-				w = it.next();
-				writer.println("<" + w + ">" + key + "</" + w + ">");
-			}
-			this.doc.put(key, word);
-		}else { //Probe the document and insert to the dictionary
-			if(this.doc.get(key) != null)
-				writer.println("<" + word + ">" + key + "</" + word +
-						">");
-			this.dict.put(key, word);
-		}
-	}
-	
-	/**
-	 * Double-pipelined hash join
-	 * @param dict dictionary filename
-	 * @param doc document filename
-	 * @param out output filename
-	 */
-	public void doublePipelinedHashJoin(String dict, String doc, String out) {
-		boolean readFromDict = true;
-		try {
-			this.writer = new PrintWriter(new OutputStreamWriter
-					(new FileOutputStream(out), FMT));
-			InputStreamReader in = new InputStreamReader(new FileInputStream(dict), FMT);
-			CSVReader reader = new CSVReader(in);
-			String[] dictLine;
-			Scanner sc = new Scanner(new FileInputStream(doc), FMT);
-			String docWord;
-			while(sc.hasNext()) { //Double-pipelined hash join
-				if(readFromDict){
-					if((dictLine = reader.readNext()) == null) break;
-					
-					if(dictLine[1].length() == 0) //Case 1: nominal term
-						this.probeAndInsert(dictLine[CHN_IDX], dictLine[0], 1);
-					else //Case 2: whole sentence
-						this.probeAndInsert(dictLine[CHN_IDX], 
-								dictLine[1].replace("~", dictLine[0]), 1);
-					readFromDict = false;
-				}else {
-					docWord = sc.next();
-					if(docWord.contains("/n")) {
-						docWord = docWord.substring(0, docWord.indexOf('/'));
-						if(doc.length() != 0)
-							this.probeAndInsert(docWord, docWord, 0);
-					}
-					readFromDict = true;
-				}
-			}
-			while((dictLine = reader.readNext()) != null) {
-				this.probeAndInsert(dictLine[CHN_IDX], dictLine[0], 1);
-			}
-			reader.close();
-			while(sc.hasNext()) {
-				docWord = sc.next();
-				if(docWord.contains("/n")) {
-					docWord = docWord.substring(0, docWord.indexOf('/'));
-					if(doc.length() != 0)
-						this.probeAndInsert(docWord, docWord, 0);
-				}
-			}
-			sc.close();
-			this.writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Simple hash join method; for testing purpose only
-	 * @param dict dictionary filename
-	 * @param doc document filename
-	 * @param out output filename
-	 */
-	public void simpleHashJoin (String dict, String doc, String out) {
-		try {
-			this.writer = new PrintWriter(new OutputStreamWriter
-					(new FileOutputStream(out), FMT));
-			InputStreamReader in = new InputStreamReader(new FileInputStream(dict), FMT);
-			CSVReader reader = new CSVReader(in);
-			String[] dictLine;
-			Scanner sc = new Scanner(new FileInputStream(doc), FMT);
-			String docWord;
-			while((dictLine = reader.readNext()) != null) {
-				this.dict.put(dictLine[CHN_IDX], dictLine[0]);
-			}
-			reader.close();
-			while(sc.hasNext()) {
-				docWord = sc.next();
-				if(docWord.contains("/n")) {
-					docWord = docWord.substring(0, docWord.indexOf('/'));
-					if(doc.length() != 0)
-						this.probeAndInsert(docWord, docWord, 0);
-				}
-			}
-			sc.close();
-			this.writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void loadDict(String dict) {
-		try {
-			InputStreamReader in = new InputStreamReader(new FileInputStream(dict), FMT);
-			CSVReader reader = new CSVReader(in);
-			int[] freq = new int[FREQ_CAP];
-			String[] dictLine;
-			while((dictLine = reader.readNext()) != null) {
-				this.dict.put(dictLine[CHN_IDX], dictLine[0]);
-				freq[dictLine[CHN_IDX].length()]++;
-			}
-			reader.close();
-			printHistogram(freq);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void printHistogram(int[] arr) {
-		for(int i = 1; i < arr.length; i++) {
-			System.out.println(i + ": " + arr[i]);
-		}
-	}
-	
-	/**
-	 * Main method to run hash join
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		hashJoin hj = new hashJoin();
-		hj.loadDict(args[0]);
-	}
+  static final int SIZE = 2003;
+  static final int FREQ_CAP = 35;
+  static final int CHN_IDX = 2;
+  static final int OUTPUT_IDX = 2;
+  static final String FMT = "UTF-8";
+
+  // private Multimap dict;
+  private HashMultimap<String, String> dict;
+  private Hashtable<String, String> doc;
+  private PrintWriter writer;
+
+  /**
+   * Default constructor
+   */
+  public hashJoin() {
+    // this.dict = new Multimap();
+    this.dict = HashMultimap.create();
+    this.doc = new Hashtable<String, String>(SIZE);
+  }
+
+  /**
+   * Helper method to probe and insert data
+   * 
+   * @param key - Chinese word
+   * @param word - English word
+   * @param option - 0 to insert to the document hashtable; 1 to insert to the
+   *          dictioanry hashtable
+   */
+  private void probeAndInsert(String key, String word, int option) {
+    if (option == 0) { // Probe the dictionary and insert to document
+      Set<String> engWords = this.dict.get(key);
+      Iterator<String> it = engWords.iterator();
+      String w;
+      while (it.hasNext()) {
+        w = it.next();
+        writer.println("<" + w + ">" + key + "</" + w + ">");
+      }
+      this.doc.put(key, word);
+    } else { // Probe the document and insert to the dictionary
+      if (this.doc.get(key) != null)
+        writer.println("<" + word + ">" + key + "</" + word + ">");
+      this.dict.put(key, word);
+    }
+  }
+
+  /**
+   * Double-pipelined hash join
+   * 
+   * @param dict - dictionary filename
+   * @param doc - document filename
+   * @param out - output filename
+   */
+  public void doublePipelinedHashJoin(String dict, String doc, String out) {
+    boolean readFromDict = true;
+    try {
+      this.writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(out), FMT));
+      InputStreamReader in = new InputStreamReader(new FileInputStream(dict), FMT);
+      CSVReader reader = new CSVReader(in);
+      String[] dictLine;
+      Scanner sc = new Scanner(new FileInputStream(doc), FMT);
+      String docWord;
+      while (sc.hasNext()) { // Double-pipelined hash join
+        if (readFromDict) {
+          if ((dictLine = reader.readNext()) == null)
+            break;
+
+          if (dictLine[1].length() == 0) // Case 1: nominal term
+            this.probeAndInsert(dictLine[CHN_IDX], dictLine[0], 1);
+          else // Case 2: whole sentence
+            this.probeAndInsert(dictLine[CHN_IDX], dictLine[1].replace("~", dictLine[0]), 1);
+          readFromDict = false;
+        } else {
+          docWord = sc.next();
+          if (docWord.contains("/n")) {
+            docWord = docWord.substring(0, docWord.indexOf('/'));
+            if (doc.length() != 0)
+              this.probeAndInsert(docWord, docWord, 0);
+          }
+          readFromDict = true;
+        }
+      }
+      while ((dictLine = reader.readNext()) != null) {
+        this.probeAndInsert(dictLine[CHN_IDX], dictLine[0], 1);
+      }
+      reader.close();
+      while (sc.hasNext()) {
+        docWord = sc.next();
+        if (docWord.contains("/n")) {
+          docWord = docWord.substring(0, docWord.indexOf('/'));
+          if (doc.length() != 0)
+            this.probeAndInsert(docWord, docWord, 0);
+        }
+      }
+      sc.close();
+      this.writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Simple hash join method; for testing purpose only
+   * 
+   * @param - dict dictionary filename
+   * @param - doc document filename
+   * @param - out output filename
+   */
+  public void simpleHashJoin(String dict, String doc, String out) {
+    try {
+      this.writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(out), FMT));
+      InputStreamReader in = new InputStreamReader(new FileInputStream(dict), FMT);
+      CSVReader reader = new CSVReader(in);
+      String[] dictLine;
+      Scanner sc = new Scanner(new FileInputStream(doc), FMT);
+      String docWord;
+      while ((dictLine = reader.readNext()) != null) {
+        this.dict.put(dictLine[CHN_IDX], dictLine[0]);
+      }
+      reader.close();
+      while (sc.hasNext()) {
+        docWord = sc.next();
+        if (docWord.contains("/n")) {
+          docWord = docWord.substring(0, docWord.indexOf('/'));
+          if (doc.length() != 0)
+            this.probeAndInsert(docWord, docWord, 0);
+        }
+      }
+      sc.close();
+      this.writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void loadDict(String dict) {
+    try {
+      InputStreamReader in = new InputStreamReader(new FileInputStream(dict), FMT);
+      CSVReader reader = new CSVReader(in);
+      int[] freq = new int[FREQ_CAP];
+      String[] dictLine;
+      while ((dictLine = reader.readNext()) != null) {
+        this.dict.put(dictLine[CHN_IDX], dictLine[0]);
+        freq[dictLine[CHN_IDX].length()]++;
+      }
+      reader.close();
+      printHistogram(freq);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void printHistogram(int[] arr) {
+    for (int i = 1; i < arr.length; i++) {
+      System.out.println(i + ": " + arr[i]);
+    }
+  }
+
+  /**
+   * Main method to run hash join
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    hashJoin hj = new hashJoin();
+    hj.loadDict(args[0]);
+  }
 }
