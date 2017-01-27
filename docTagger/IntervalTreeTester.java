a6653e2ea7d22e2cd3f23416aa795068bb0d9a2e
Haoran Sun <hsun960120@gmail.com>
2017-01-27 02:11:16 -0800

    Format fix

--- docTagger/IntervalTreeTester.java
+++ docTagger/IntervalTreeTester.java
@@ -4,39 +4,41 @@
  * @since 01/26/2017
  */
 package docTagger;
+
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class IntervalTreeTester {
-	private IntervalTree<Interval> it;
+  private IntervalTree<Interval> it;
 
-	/**
-	 * Set up testing fixture for
-	 * @throws Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-		this.it = new IntervalTree<>();
-		this.it.insert(new IndexInterval(0, 4));
-		this.it.insert(new IndexInterval(5, 7));
-		this.it.insert(new IndexInterval(10, 12));
-		this.it.insert(new IndexInterval(18, 20));
-		this.it.insert(new IndexInterval(15, 17));
-	}
+  /**
+   * Set up testing fixture for
+   * 
+   * @throws Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+    this.it = new IntervalTree<>();
+    this.it.insert(new IndexInterval(0, 4));
+    this.it.insert(new IndexInterval(5, 7));
+    this.it.insert(new IndexInterval(10, 12));
+    this.it.insert(new IndexInterval(18, 20));
+    this.it.insert(new IndexInterval(15, 17));
+  }
 
-	/**
-	 * Test query overlapping interval
-	 */
-	@Test
-	public void testQuery() {
-		assertTrue(it.contains(new IndexInterval(10, 12)));
-		assertFalse(it.contains(new IndexInterval(0, 12)));
-		
-		assertEquals(8, it.nextAvailable(new IndexInterval(5, 6)));
-		assertEquals(13, it.nextAvailable(new IndexInterval(8, 12)));
-		assertEquals(8, it.nextAvailable(new IndexInterval(4, 6)));
-	}
+  /**
+   * Test query overlapping interval
+   */
+  @Test
+  public void testQuery() {
+    assertTrue(it.contains(new IndexInterval(10, 12)));
+    assertFalse(it.contains(new IndexInterval(0, 12)));
+
+    assertEquals(8, it.nextAvailable(new IndexInterval(5, 6)));
+    assertEquals(13, it.nextAvailable(new IndexInterval(8, 12)));
+    assertEquals(8, it.nextAvailable(new IndexInterval(4, 6)));
+  }
 
 }
